885245c9f27a53511ace777419f5700c4e27e7f7
Lucas Venturella <lucas.venturella@getambush.com>
2020-11-22 12:38:59 -0300

    Add project files.

---
+++ ImageDetector/ImageNetData.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ImageDetector
+{
+    public class ImageNetData
+    {
+        [LoadColumn(0)] public string ImagePath;
+        [LoadColumn(1)] public string Label;
+
+        /// <summary>
+        /// Load the contents of a TSV file as an object sequence representing images and labels
+        /// </summary>
+        /// <param name="file">Name of the TSV file</param>
+        /// <returns>sequence of objects containing result of TSV</returns>
+        public static IEnumerable<ImageNetData> ReadFromCsv(string file)
+        {
+            return File.ReadAllLines(file)
+                .Select(x => x.Split('\t'))
+                .Select(x => new ImageNetData
+                {
+                    ImagePath = x[0],
+                    Label = x[1]
+                });
+        }
+    }
+
+    /// <summary>
+    /// prediction class that holds a model prediction
+    /// </summary>
+    public class ImageNetPrediction
+    {
+        [ColumnName("softmax2")]
+        public float[] PredictLabels;
+    }
+}
---
+++ ImageDetector/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.ML;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace ImageDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a machine learning context
+            var mlContext = new MLContext();
+
+            //load the tsv file with data
+            var data = mlContext.Data.LoadFromTextFile<ImageNetData>("./images/tags.tsv", hasHeader: true);
+
+            var pipeline = mlContext.Transforms
+                .LoadImages(
+                outputColumnName: "input",
+                imageFolder: "images",
+                inputColumnName: nameof(ImageNetData.ImagePath))
+                .Append(mlContext.Transforms
+                .ResizeImages(
+                    outputColumnName: "input",
+                    imageWidth: 224,
+                    imageHeight: 224,
+                    inputColumnName: "input"))
+                .Append(mlContext.Transforms.ExtractPixels(
+                    outputColumnName: "input", 
+                    interleavePixelColors: true, 
+                    offsetImage: 117))
+                .Append(mlContext.Model.LoadTensorFlowModel("./models/tensorflow_inception_graph.pb")
+                .ScoreTensorFlowModel(
+                    outputColumnNames: new[] { "softmax2" },
+                    inputColumnNames: new[] { "input" },
+                    addBatchDimensionInput: true));
+
+            Console.WriteLine("Start training model...");
+            var model = pipeline.Fit(data);
+            Console.WriteLine("Model training complete");
+
+
+            var engine = mlContext.Model.CreatePredictionEngine<ImageNetData, ImageNetPrediction>(model);
+
+            var labels = File.ReadAllLines("models/imagenet_comp_graph_label_strings.txt");
+
+            Console.WriteLine("Predicting image contents...");
+            var images = ImageNetData.ReadFromCsv("images/tags.tsv");
+            foreach (var image in images)
+            {
+                Console.WriteLine($" [{image.ImagePath}]: ");
+                var prediction = engine.Predict(image).PredictLabels;
+
+                var i = 0;
+                var best = (from p in prediction
+                            select new { Index = i++, Prediction = p })
+                            .OrderByDescending(p => p.Prediction)
+                            .First();
+                var predictedLabel = labels[best.Index];
+
+                Console.Write($"{predictedLabel} {(predictedLabel != image.Label ? "**WRONG**" : "")}");
+            }
+        }
+    }
+}
